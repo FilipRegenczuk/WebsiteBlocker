8baf15212fd70f217c7112f0c77c355f32456c1e
FilipRegenczuk <f.regenczuk@outlook.com>
2019-10-14 22:02:15 +0200

    while loop + checking time

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
+import time
+from datetime import datetime as dt
 
 hosts_path = r"C:\Windows\System32\drivers\etc\hosts"
-address = "127.0.0.1"
-blocked_websites = ["www.facebook.com", "facebook.com"]
+redirect_address = "127.0.0.1"
+blocked_websites = ["www.youtube.com", "youtube.com"]
+
+while True:
+    if dt(dt.now().year, dt.now().month, dt.now().day, 8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 16):
+        print("Working")
+    else:
+        print("Free time")
+    
+    time.sleep(2)
